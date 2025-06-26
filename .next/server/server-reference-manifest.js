8be92e2efa396f87c136ea938a5e8698bd343c49
aminealamaa <162344514+aminealamaa@users.noreply.github.com>
2025-06-26 11:45:21 +0200

    Updated postcss.config.js

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"syRcCd2pmbGmbQpz4SWrYqZOY8QAuuXpCnNW4zlM/bU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"U5pRDMtbPffEtNJ32G1I83ntuS0ZkQwhdeyQu3PLbks=\"\n}"
